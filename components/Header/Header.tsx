51b19a6b7d385b2a2c914774256d4aac4b60c1e9
Tom Schneider <tom.schneider@willowtreeapps.com>
2023-03-23 15:29:45 -0400

    Initial

---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "../Logo";
+import { Nav } from "../Nav";
+
+export default function Header() {
+    return (
+        <header>
+            <Logo></Logo>
+            <h1>Tom Schneider</h1>
+            <Nav></Nav>
+        </header>
+    )
+}
---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "../Footer"
+import { Header } from "../Header"
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <Header></Header>
+            <main>{children}</main>
+            <Footer></Footer>
+        </div>)
+}
