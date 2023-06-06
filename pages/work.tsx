32d835d2e36c5f6b9eeb881122a69ca4698da217
Tom Schneider <tom.schneider@willowtreeapps.com>
2023-06-06 16:54:22 -0400

    removed unused code

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { Layout } from '../components/Layout'
 import { ContentBox } from '../components/ContentBox'
-import { Image } from '../components/Image'
 
 export { getServerSideProps } from './../api/pages/work'
 
@@ -14,7 +13,7 @@ export default function ResumePage({ title, mobileTitle, tagline, content, image
       </Head>
       <Layout title={title} mobileTitle={mobileTitle} tagline={tagline} accent={accent} footer={footer}>
         {companies.map(company => {
-          const src = company.image.props.src
+          // const src = company.image.props.src
           return (
             <ContentBox minHeight={false} key={company._key} count={1} headline={company.company}>
               {/* <Image fill={false} width={400} height={200} src={src} classes="blockImage" alt={company.company} /> */}
