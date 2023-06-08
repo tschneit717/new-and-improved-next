ccda4706e901afc2bb8cb5336a458b84c1dd45e6
Tom Schneider <tom.schneider@willowtreeapps.com>
2023-06-08 10:57:45 -0400

    fix this later

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -10,6 +10,7 @@ Form.HoneyPot = HoneyPot
 
 export default function Form ({ children, isNetlifyForm, action, onSubmit, name }: FormProps): JSX.Element {
   return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form className={styles.form} onSubmit={onSubmit} method="POST" name={name} action={action} data-netlify={isNetlifyForm} data-netlify-honeypot={isNetlifyForm ? 'bot-field' : ''}>
       {isNetlifyForm && <input type="hidden" name="form-name" value={name} />}
       {children}
